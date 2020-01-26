ae2fe358d45be288ec3aec40e11595097c8d3f08
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-01-26 17:03:26 +0800

    Create codepen.js

---
+++ codepen.js
@@ -0,0 +1,7 @@
+import SVGMark from 'https://cloudfrank.github.io/SvgMark/SVGMark.js';
+customElements.define('svg-mark', SVGMark, { extends: "div"});
+import SVGMarks from 'https://cloudfrank.github.io/SvgMark/SVGMarks.js';
+customElements.define('svg-marks', SVGMarks, { extends: "div"});
+
+import CloudFrankLogomark from 'https://cloudfrank.github.io/SvgMark/CloudFrankLogomark.js';
+customElements.define('cloud-frank-logomark', CloudFrankLogomark, { extends: "div"});
