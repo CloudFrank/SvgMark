f84de8ac2fead91f6a05a80f01af5ca98dbce3ec
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-01-26 15:52:53 +0800

    Create SVGMark.js

---
+++ SVGMark.js
@@ -0,0 +1,42 @@
+class SVGMark extends HTMLDivElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.render();
+  }
+  
+  render() {
+    this.shadowRoot.innerHTML = this.template();
+  }
+  
+  template() {
+    return `
+      <slot></slot>
+      ${this.style()}
+    `;
+  }
+  
+  style() {
+    return `
+      <style>
+        :host {
+          display: grid;
+          grid-template: auto / auto;
+          align-items: center;
+          justify-items: center;
+          position: relative;
+        }
+        ::slotted(svg) {
+          display: block;
+          width: 100%;
+          height: 100%;
+          fill: #f9f9f9;
+          position: absolute;
+        }
+
+      </style>
+    `;
+  }
+};
+
+//customElements.define('svg-mark', SVGMark, { extends: "div"});
