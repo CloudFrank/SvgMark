023d2ff12a2e5d5dc57737cce3bf10deca2ee349
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-01-26 20:57:20 +0800

    Update codepen.js

--- codepen.js
+++ codepen.js
@@ -5,3 +5,10 @@ customElements.define('svg-marks', SVGMarks, { extends: "div"});
 
 import CloudFrankLogomark from 'https://cloudfrank.github.io/SvgMark/CloudFrankLogomark.js';
 customElements.define('cloud-frank-logomark', CloudFrankLogomark, { extends: "div"});
+import CloudFrankLogotype from 'https://cloudfrank.github.io/SvgMark/CloudFrankLogotype.js';
+customElements.define('cloud-frank-logotype', CloudFrankLogotype, { extends: "div"});
+
+import WordRinkLogomark from 'https://cloudfrank.github.io/SvgMark/WordRinkLogomark.js';
+customElements.define('word-rink-logomark', WordRinkLogomark, { extends: "div"});
+import WordRinkLogotype from 'https://cloudfrank.github.io/SvgMark/WordRinkLogotype.js';
+customElements.define('word-rink-logotype', WordRinkLogotype, { extends: "div"});
